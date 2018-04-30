a5546169b47877844eb57ca5ce26a41561559897
Steve Baker <ste.baker.92@gmail.com>
2018-04-30 17:05:29 +0100

    Added routing

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -2,26 +2,27 @@
   <div id="app">
     <img src="./assets/logo.png">
     <HelloWorld msg="Phoenix"/>
-    <BookingList />
+    <router-view></router-view>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import BookingList from './components/BookingList.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import BookingList from "./components/BookingList.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld,
     BookingList
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- vue-app/src/main.js
+++ vue-app/src/main.js
@@ -1,8 +1,35 @@
 import Vue from 'vue'
+import VueRouter from "vue-router"
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// 0. If using a module system (e.g. via vue-cli), import Vue and VueRouter
+// and then call `Vue.use(VueRouter)`.
+Vue.use(VueRouter)
+// 1. Define route components.
+// These can be imported from other files
+import BookingList from "./components/BookingList"
+
+// 2. Define some routes
+// Each route should map to a component. The "component" can
+// either be an actual component constructor created via
+// `Vue.extend()`, or just a component options object.
+// We'll talk about nested routes later.
+const routes = [
+  { path: '/', component: BookingList },
+  // { path: '/bar', component: Bar }
+]
+
+// 3. Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
+const router = new VueRouter({
+  routes // short for `routes: routes`
+})
+
+
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
